bcbf980cf16e1ec447e318c2d29b52c52d3de16d
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-11-23 16:54:58 +0100

    Svelte project opgezet + basic styling toegevoegd

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+<style>    
+    @font-face {
+        font-family: 'Glory';
+        src: url('$lib/assets/fonts/Glory-regular.ttf') format('truetype');
+    }
+</style>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<nav>
+    <a href="/about">About me</a>
+    <a href="/work">See my work</a>
+    <a href="/experiments">See my experiments</a>
+    <a href="/contact">Get in contact</a>
+</nav>
+
+<h1>Anouk de Rooij</h1>
+<p>Frontend developer portfolio</p>
